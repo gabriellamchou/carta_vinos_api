cd2ee654a20f38f00557639e5a9de83e86eda7cf
Gabriel <gabriel.lamela.ch@gmail.com>
2024-05-27 12:15:04 +0200

    Añadir imágenes a la respuesta de obtener_vino
    Añadir join a la tabla imagen_vino
    Recorrer el array de filas que devuelve la petición get
    Agrupar todas las imágenes del vino en un array y devolver un solo vino en lugar de tantos resultados de vino como imágenes tenga el mismo

--- application/models/VinoModel.php
+++ application/models/VinoModel.php
@@ -80,6 +80,7 @@ class VinoModel extends CI_Model
             v.IdRegion AS RegionId,
             r.Nombre AS RegionNombre,
             r.Pais AS RegionPais,
+            r.Descripcion AS RegionDescripcion,
             v.IdBodega AS BodegaId,
             b.Nombre AS BodegaNombre,
             b.Descripcion AS BodegaDescripcion,
@@ -88,19 +89,31 @@ class VinoModel extends CI_Model
             v.Graduacion,
             v.BreveDescripcion,
             v.Capacidad,
-            v.Stock"
+            v.Stock,
+            i.ImagenPath"
         );
         $this->db->from("vino AS v");
         $this->db->where("v.Id", $id);
         $this->db->join("tipo AS t", "v.IdTipoVino = t.Id", "left");
         $this->db->join("region AS r", "v.IdRegion = r.Id", "left");
         $this->db->join("bodega AS b", "v.IdBodega = b.Id", "left");
+        $this->db->join("imagen_vino AS i", "i.IdVino = v.Id", "left");
 
         $query = $this->db->get();
 
-        $rows = $query->result();
+        $rows = $query->result_array();
 
-        return ($rows);
+        foreach ($rows as $row) {
+            if (!isset($result[$row['Id']])) {
+                $result[$row['Id']] = $row;
+                $result[$row['Id']]['Imagenes'] = [];
+            }
+            if ($row['ImagenPath']) {
+                $result[$row['Id']]['Imagenes'][] = $row['ImagenPath'];
+            }
+        }
+
+        return array_values($result);
     }
 
     # Inserta un vino
